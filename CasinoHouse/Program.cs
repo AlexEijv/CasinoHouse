2e7657f468f023569c261851eea9777a1784e4ae
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2024-04-09 10:22:02 +0200

    aerhg

--- CasinoHouse/Program.cs
+++ CasinoHouse/Program.cs
@@ -24,6 +24,7 @@ while (place != "streets" && free)
         string go = Console.ReadLine();
         go = go.ToLower();
 
+
         if (go == "blackjack")
         {
             money = Blackjack.PlayBlackjack(money);
